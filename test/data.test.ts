dc393fb13e8c7a085c9a26ca19b4a1c7b7561b5f
Jackson Taylor <jacksoneliastaylor@gmail.com>
2023-09-01 14:40:04 -0400

    repo cleanup

--- test/data.test.ts
+++ test/data.test.ts
@@ -1,92 +1,92 @@
 import { describe, expect, test } from '@jest/globals';
 
-import { Challenge, ChallengeDocument, LeaderboardData, Participant, ParticipantDocument } from './data';
+import { Challenge, ChallengeDocument, LeaderboardData, Participant, ParticipantDocument } from '../src/data';
 import { DocumentSnapshot } from 'firebase/firestore';
-import { dateToTimestamp } from './util';
+import { dateToTimestamp } from '../src/util';
 
 describe('test Challenge functionality', () => {
   describe('isCompleted method', () => {
-  const doc: Partial<DocumentSnapshot<ChallengeDocument>> = {
-    id: '1',
-    data: () => ({
-      ownerId: '1',
-      name: 'test',
-      startDate: dateToTimestamp(new Date('2023-07-01 00:00:00'))!,
-      dayCount: 5,
-      users: [],
-    }),
-  };
-  const challenge = new Challenge(doc as any);
+    const doc: Partial<DocumentSnapshot<ChallengeDocument>> = {
+      id: '1',
+      data: () => ({
+        ownerId: '1',
+        name: 'test',
+        startDate: dateToTimestamp(new Date('2023-07-01 00:00:00'))!,
+        dayCount: 5,
+        users: [],
+      }),
+    };
+    const challenge = new Challenge(doc as any);
 
-  test('date prior to start date', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-06-31 23:59:59'));
-    expect(challenge.isCompleted()).toBe(false);
-  });
+    test('date prior to start date', () => {
+      jest.useFakeTimers().setSystemTime(new Date('2023-06-31 23:59:59'));
+      expect(challenge.isCompleted()).toBe(false);
+    });
 
-  test('first day of challenge', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-07-01 00:00:00'));
-    expect(challenge.isCompleted()).toBe(false);
-  });
+    test('first day of challenge', () => {
+      jest.useFakeTimers().setSystemTime(new Date('2023-07-01 00:00:00'));
+      expect(challenge.isCompleted()).toBe(false);
+    });
 
-  test('date during challenge', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-07-03 03:14:39'));
-    expect(challenge.isCompleted()).toBe(false);
-  });
+    test('date during challenge', () => {
+      jest.useFakeTimers().setSystemTime(new Date('2023-07-03 03:14:39'));
+      expect(challenge.isCompleted()).toBe(false);
+    });
 
-  test('last day of challenge', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-07-05 23:59:59'));
-    expect(challenge.isCompleted()).toBe(false);
-  });
+    test('last day of challenge', () => {
+      jest.useFakeTimers().setSystemTime(new Date('2023-07-05 23:59:59'));
+      expect(challenge.isCompleted()).toBe(false);
+    });
 
-  test('date after challenge', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-07-06 00:00:01'));
-    expect(challenge.isCompleted()).toBe(true);
+    test('date after challenge', () => {
+      jest.useFakeTimers().setSystemTime(new Date('2023-07-06 00:00:01'));
+      expect(challenge.isCompleted()).toBe(true);
+    });
   });
-});
 
   describe('currentDay method', () => {
-  const doc: Partial<DocumentSnapshot<ChallengeDocument>> = {
-    id: '1',
-    data: () => ({
-      ownerId: '1',
-      name: 'test',
-      startDate: dateToTimestamp(new Date('2023-07-01 00:00:00'))!,
-      dayCount: 5,
-      users: [],
-    }),
-  };
-  const challenge = new Challenge(doc as any);
+    const doc: Partial<DocumentSnapshot<ChallengeDocument>> = {
+      id: '1',
+      data: () => ({
+        ownerId: '1',
+        name: 'test',
+        startDate: dateToTimestamp(new Date('2023-07-01 00:00:00'))!,
+        dayCount: 5,
+        users: [],
+      }),
+    };
+    const challenge = new Challenge(doc as any);
 
-  test('date prior to start date', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-06-18 23:59:59'));
-    expect(challenge.currentDay()).toBe(0);
-  });
+    test('date prior to start date', () => {
+      jest.useFakeTimers().setSystemTime(new Date('2023-06-18 23:59:59'));
+      expect(challenge.currentDay()).toBe(0);
+    });
 
-  test('just before start date', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-06-31 23:59:59'));
-    expect(challenge.currentDay()).toBe(0);
-  });
+    test('just before start date', () => {
+      jest.useFakeTimers().setSystemTime(new Date('2023-06-31 23:59:59'));
+      expect(challenge.currentDay()).toBe(0);
+    });
 
-  test('first day of challenge', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-07-01 00:00:01'));
-    expect(challenge.currentDay()).toBe(1);
-  });
+    test('first day of challenge', () => {
+      jest.useFakeTimers().setSystemTime(new Date('2023-07-01 00:00:01'));
+      expect(challenge.currentDay()).toBe(1);
+    });
 
-  test('date during challenge', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-07-03 03:14:39'));
-    expect(challenge.currentDay()).toBe(3);
-  });
+    test('date during challenge', () => {
+      jest.useFakeTimers().setSystemTime(new Date('2023-07-03 03:14:39'));
+      expect(challenge.currentDay()).toBe(3);
+    });
 
-  test('last day of challenge', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-07-05 23:59:59'));
-    expect(challenge.currentDay()).toBe(5);
-  });
+    test('last day of challenge', () => {
+      jest.useFakeTimers().setSystemTime(new Date('2023-07-05 23:59:59'));
+      expect(challenge.currentDay()).toBe(5);
+    });
 
-  test('date after challenge', () => {
-    jest.useFakeTimers().setSystemTime(new Date('2023-07-06 00:00:01'));
-    expect(challenge.currentDay()).toBe(5);
+    test('date after challenge', () => {
+      jest.useFakeTimers().setSystemTime(new Date('2023-07-06 00:00:01'));
+      expect(challenge.currentDay()).toBe(5);
+    });
   });
-});
 });
 
 describe('test LeaderboardData functionality', () => {
@@ -190,6 +190,6 @@ describe('test LeaderboardData functionality', () => {
       expect(leaderboardData1.lineChart).toEqual([0, 1, 0, 1, 0, 1, 0, 1]);
       expect(leaderboardData2.lineChart).toEqual([1, 2, 3, 0, 0, 0, 0, 0]);
       expect(leaderboardData3.lineChart).toEqual([0, 0, 1, 0, 0, 1, 2, 3]);
-    })
+    });
   });
 });
