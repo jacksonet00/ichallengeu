deec78fdc5be33ee2b92216479c9ee1f92863f56
Jackson Taylor <jacksoneliastaylor@gmail.com>
2023-09-01 14:49:16 -0400

    add rewrites to next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const __PROD__ = process.env.NODE_ENV === 'production';
+
+const destinationHost = __PROD__ ? 'https://ichallengeu.app' : 'http://localhost:3000';
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -9,9 +14,30 @@ const nextConfig = {
       },
     ],
   },
-  // async rewrites() {
-  //   return [];
-  // }
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/:path*",
+      },
+      {
+        source: "/favicon.ico",
+        destination: `${destinationHost}/favicon.ico`,
+      },
+      {
+        source: "/chevron-down.svg",
+        destination: `${destinationHost}/chevron-down.svg`,
+      },
+      {
+        source: "/chevron-left.svg",
+        destination: `${destinationHost}/chevron-left.svg`,
+      },
+      {
+        source: "/frozen-fire.svg",
+        destination: `${destinationHost}/frozen-fire.svg`,
+      },
+    ];
+  }
 }
 
 module.exports = nextConfig
